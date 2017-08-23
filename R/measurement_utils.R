3f46a52b3c682a6a835667112d05bb1f9b8f71e7
noerw <noerw@gmx.de>
2017-08-23 16:12:03 +0200

    show units in measurements plot

--- R/measurement_utils.R
+++ R/measurement_utils.R
@@ -1,6 +1,8 @@
 #' @export
 plot.osem_measurements = function (x, ...) {
-  # TODO: group/color by sensor_id
-  plot(value~createdAt, x, col = x$sensorId, ...)
+  oldpar = par()
+  par(mar = c(2,4,1,1))
+  plot(value~createdAt, x, col = x$sensorId, xlab = NA, ylab = x$unit[1], ...)
+  par(mar = oldpar$mar)
   invisible(x)
 }
