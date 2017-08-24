51595f12393dfe3392c96554b9719910691244f7
noerw <noerw@gmx.de>
2017-08-24 13:57:15 +0200

    make plot.osem_measurements() more robust

--- R/measurement_utils.R
+++ R/measurement_utils.R
@@ -2,7 +2,7 @@
 plot.osem_measurements = function (x, ...) {
   oldpar = par()
   par(mar = c(2,4,1,1))
-  plot(value~createdAt, x, col = x$sensorId, xlab = NA, ylab = x$unit[1], ...)
+  plot(value~createdAt, x, col = factor(x$sensorId), xlab = NA, ylab = x$unit[1], ...)
   par(mar = oldpar$mar)
   invisible(x)
 }
