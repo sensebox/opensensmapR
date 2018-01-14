542f2205eb44ba2d71aad471676d33db56073815
nuest <daniel.nuest@uni-muenster.de>
2018-01-14 21:49:51 +0100

    add tests for phenomena and box

---
+++ tests/testthat/test_box.R
@@ -0,0 +1,30 @@
+context("box")
+
+check_api <- function() {
+  code <- NA
+  try(code <- httr::status_code(httr::GET(osem_endpoint())))
+  if (is.na(code)) skip("API not available")
+}
+
+try({
+  boxes <- osem_boxes()
+})
+
+test_that("a box can be converted to sf object", {
+  check_api()
+  
+  box <- osem_box(boxes$X_id[[1]])
+  box_sf <- sf::st_as_sf(box)
+  
+  expect_true(sf::st_is_simple(box_sf))
+  expect_true("sf" %in% class(box_sf))
+})
+
+test_that("a box converted to sf object keeps all attributes", {
+  check_api()
+  
+  box <- osem_box(boxes$X_id[[1]])
+  box_sf <- sf::st_as_sf(box)
+  
+  expect_true(all(names(box) %in% names(box_sf)))
+})
